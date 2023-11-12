c26dc1ea9c6c0bb99c2605ec847d0eb3aa77e55a
shusaku009 <shusaku009@gmail.com>
2023-11-12 13:50:32 +0900

    add: メッセージ表示機能の実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import { useCallback, useState } from "react"
 import axios from "axios";
 import { User } from "../types/api/user";
 import { useHistory } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   
   const [loading, setLoding] = useState(false);
 
@@ -13,12 +15,13 @@ export const useAuth = () => {
 
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) {
+        showMessage({ title: "ログインしました", status: "success" });
         history.push("/home");
       } else {
-        alert("ユーザーが見つかりません")
+        showMessage({ title: "ユーザーが見つかりません", status: "error" });
       }
-    }).catch(() => alert("ログインできません")).finally(() => setLoding(false));
+    }).catch(() => showMessage({ title: "ログインできません", status: "error" })).finally(() => setLoding(false));
   }, 
-  [history]);
+  [history, showMessage]);
   return { login, loading };
 }
