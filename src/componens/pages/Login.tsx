7086e23c99d429cfd7d5eed4bac576aa552154f6
shusaku009 <shusaku009@gmail.com>
2023-11-15 16:14:03 +0900

    add: 管理者ユーザーのみユーザー情報を編集できるように機能追加

--- src/componens/pages/Login.tsx
+++ src/componens/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, memo, useState, VFC } from "react"
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { PramaryButton } from "../atoms/button/PrimaryButton";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { useAuth } from "../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
@@ -21,7 +21,7 @@ export const Login: VFC = memo(() => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザーID" value={userId} onChange={onChangeUserId} />
-          <PramaryButton disabled={userId === "" } loading={loading} onClick={onClickLogin}>ログイン</PramaryButton>
+          <PrimaryButton disabled={userId === "" } loading={loading} onClick={onClickLogin}>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
--- src/componens/pages/UserManagement.tsx
+++ src/componens/pages/UserManagement.tsx
@@ -44,7 +44,7 @@ export const UserManagement: VFC = memo(() => {
           })}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal user={selectedUser} isOpen={isOpen} isAdmin={loginUser?.isAdmin} onClose={onClose} />
     </>
   );
 });
